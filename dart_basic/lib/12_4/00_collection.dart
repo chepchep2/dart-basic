02bd33e79400428c756618e74d7a92013f4806ff
sangwoo <kajer102@gmail.com>
2023-12-04 21:24:01 +0900

    연습문제 풀기전 복습1

--- dart_basic/lib/12_4/00_collection.dart
+++ dart_basic/lib/12_4/00_collection.dart
@@ -8,7 +8,7 @@ void main() {
   numbers.add(3);
   numbers.add(4);
   numbers.insert(0, 5);
-  print(numbers);
+  // print(numbers);
 
 // 안티패턴 1
   final numbers2 = [];
@@ -16,7 +16,7 @@ void main() {
   numbers2.add('1');
   numbers2.add(true);
   numbers2.add(null);
-  print(numbers2);
+  // print(numbers2);
 
   // 안티패턴 2
   final List numbers3 = [];
@@ -28,4 +28,51 @@ void main() {
   dynamic a = 10;
 
   a = 'ㅁㅁㅁ';
+
+  final List<String> names = [];
+  names.add('홍길동');
+  names.add('한석봉');
+  names.add('신사임당');
+
+  print(names[0]);
+
+  print('-------------');
+
+  for (int i = 0; i < names.length; i++) {
+    print(names[i]);
+  }
+
+  print('--------------');
+
+  for (final name in names) {
+    print(name);
+  }
+
+  print('--------------');
+
+  for (var e in names) {
+    print(e);
+  }
+
+  print('--------------');
+  print('--------------');
+
+  names.forEach(print);
+
+  print('--------------');
+
+  List<int> subjects = [10, 50, 100, 100, 50];
+
+  final iterator = subjects.iterator;
+  while (iterator.moveNext()) {
+    print(iterator.current);
+  }
+  print('--------------');
+
+  Set<int> lottoSet = {1, 2, 3, 4, 5, 6};
+
+  final iterator2 = lottoSet.iterator;
+  while (iterator2.moveNext()) {
+    print(iterator2.current);
+  }
 }
