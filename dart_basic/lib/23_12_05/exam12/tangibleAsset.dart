c226603e7dc98fa4ac3989e30c71f0f301e18817
sangwoo <kajer102@gmail.com>
2023-12-06 10:03:54 +0900

    설계 연습 보관필수

--- dart_basic/lib/23_12_05/exam12/tangibleAsset.dart
+++ dart_basic/lib/23_12_05/exam12/tangibleAsset.dart
@@ -3,20 +3,24 @@ import 'package:dart_basic/23_12_05/exam12/thing.dart';
 
 abstract class TangibleAsset extends Asset implements Thing {
   String color;
-  double _weight;
+  // double _weight;
+  @override
+  double weight;
 
   TangibleAsset({
     required super.name,
     required super.price,
     required this.color,
-    required double weight,
-  }) : _weight = weight;
+    // required double weight,
+    required this.weight,
+  });
+  //  : _weight = weight;
 
-  @override
-  double get weight => _weight;
+  // @override
+  // double get weight => _weight;
 
-  @override
-  set weight(double weight) {
-    _weight = weight;
-  }
+  // @override
+  // set weight(double weight) {
+  //   _weight = weight;
+  // }
 }
