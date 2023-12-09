3bbc143e5ee900258661c33687889e6dc9da9ede
sangwoo <kajer102@gmail.com>
2023-12-09 22:22:49 +0900

    String 실습2

--- dart_basic/lib/23_12_09/string_ex1.dart
+++ dart_basic/lib/23_12_09/string_ex1.dart
@@ -1,11 +1,17 @@
 // i 번째 글자가 모음인지 알려주는 isVowel() 함수를 완성하시오
 // 영어에서의 모음은 a, e, i, o, u 다섯가지이다.
 
+// i 번째 글자가 자음인지 알려주는 isConsonant() 함수를 완성하시오
+
 class Word {
   String word = '';
 
   bool isVowel(int i) {
     String vowel = word.substring(i, i + 1);
     return ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'].contains(vowel);
+  }
+
+  bool isConsonant(int i) {
+    return !isVowel(i);
   }
 }
