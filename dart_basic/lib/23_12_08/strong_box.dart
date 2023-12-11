e70902af239dbde680c00d4c573ad9f08e3fdc41
sangwoo <kajer102@gmail.com>
2023-12-12 00:12:19 +0900

    StrongBox

--- dart_basic/lib/23_12_08/strong_box.dart
+++ dart_basic/lib/23_12_08/strong_box.dart
@@ -6,8 +6,43 @@ enum KeyType {
 }
 
 class StrongBox<E> {
+  final KeyType _keyType;
   E? _item;
-  int oepn = 1;
+  int _count = 0;
+
+  StrongBox(this._keyType);
+
   void put(E item) => _item = item;
-  E? get() => _item;
+  E? get() {
+    _count++;
+    switch (_keyType) {
+      case KeyType.padlock:
+        if (_count > 1024) {
+          return _item;
+        }
+      case KeyType.button:
+        if (_count > 10000) {
+          return _item;
+        }
+      case KeyType.dial:
+        if (_count > 30000) {
+          return _item;
+        }
+      case KeyType.finger:
+        if (_count > 1000000) {
+          return _item;
+        }
+        break;
+    }
+    return null;
+  }
+}
+
+void main() {
+  final box = StrongBox<String>(KeyType.padlock);
+  box.put('임금님 귀는 당나귀 귀ㅋ');
+
+  for (int i = 0; i < 1025; i++) {
+    print('${i + 1}번째: ${box.get()}');
+  }
 }
