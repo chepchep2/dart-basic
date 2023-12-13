b09adb310b2942aeaedde21de75e59614ba7f160
sangwoo <kajer102@gmail.com>
2023-12-13 23:06:12 +0900

    연습 2

--- dart_basic/lib/23_12_13/exam/exam2.dart
+++ dart_basic/lib/23_12_13/exam/exam2.dart
@@ -9,3 +9,13 @@ Future<String> getMovieInfo() async {
   };
   return jsonEncode(mockData);
 }
+
+void main() async {
+  print(await getMovieInfo());
+  print('------------------------------');
+  // getMovieInfo가 비동기 이기때문에 async-await를 써서 출력 가능
+  final result = await getMovieInfo();
+  final result2 = jsonDecode(result);
+  print(result2);
+  // 여기서 Map형식으로 오는데 director 키의 밸류에 어떻게 접근하는지 모르겠다..
+}
